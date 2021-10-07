225511d0b28285cf7125f08a5f277c1b823e4c67
alright21 <alberto.giust@studenti.unitn.it>
2021-10-07 17:37:53 +0200

    feat: complete first ids (still not perfect)

--- src/main.py
+++ src/main.py
@@ -43,7 +43,6 @@ class CSVReader(can.io.generic.BaseIOHandler):
             date, time = timestamp.split(' ')
             year, month, day = date.split('-')
             hour, minute, seconds = time.split(':')
-
             second, microsecond = seconds.split('.')
 
             dt = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second), int(microsecond))
@@ -55,12 +54,13 @@ class CSVReader(can.io.generic.BaseIOHandler):
                     data.append(int(data_temp[i]))
             yield can.Message(
                 timestamp=dt.timestamp(),
-                is_remote_frame=(False),
+                # timestamp=float(timestamp),
+                is_remote_frame=(True if dlc=='0' else False),
                 is_extended_id=(True),
                 is_error_frame=(False),
                 arbitration_id=int(arbitration_id, base=16),
                 dlc=int(dlc),
-                data=data,
+                data=(data if dlc!='0' else None),
                 check=True
             )
 
@@ -76,10 +76,6 @@ class ProducerThread(threading.Thread):
         return
 
     def run(self):
-        # for i in range(10):
-        #     msg = can.Message(arbitration_id=0xc0ffee, data=[0, i, 0, 1, 3, 1, 4, 1], is_extended_id=False)
-        #     self.bus.send(msg)
-        #     time.sleep(1)
         i = 0
         last_timestamp = 0.0
         for msg in CSVReader('/home/alright/TURKU/thesis/data/CAN-Vehicle/2020_12_04_15_49_09_806427_vehicle.csv'):
@@ -104,13 +100,14 @@ class ConsumerThread(threading.Thread):
         min_tolerance = {}
         # max_tolerance = {}
         last_timestamp = {}
+        ignore_next_msg = {}
         logging.debug(self.name + " fired up")
         i = 0
 
-        # set up IDS with initial messages (training set)
-        while True:
-            # logging.info(i)
-            msg = self.bus.recv(10)
+        for msg in CSVReader('/home/alright/TURKU/thesis/data/OTIDS/Fuzzy_attack_dataset1.txt'):
+        # for msg in CSVReader('/home/alright/TURKU/thesis/data/OTIDS/Attack_free_dataset1.txt'):
+        # for msg in CSVReader('/home/alright/TURKU/thesis/data/CAN-Vehicle/2020_12_04_15_49_09_806427_vehicle.csv'):
+        # for msg in CSVReader('/home/alright/TURKU/thesis/data/CAN-Vehicle/2020_12_07_07_54_05_363774_vehicle.csv'):
             if msg is None:
                 logging.info('No message has been received')
                 sys.exit()
@@ -120,39 +117,27 @@ class ConsumerThread(threading.Thread):
                 # define threshold of periodicity of the message
 
                 # the arbitration_id  has already been seen
-                if msg.arbitration_id in last_timestamp:
-                    time_frame = msg.timestamp - last_timestamp[msg.arbitration_id]
-                    if msg.arbitration_id not in min_tolerance:
-                        min_tolerance[msg.arbitration_id] = time_frame
-                    else:
-                        if time_frame < (min_tolerance[msg.arbitration_id]/2):
-                            logging.error("ATTACK detected: i=" + str(i) + " " + str(msg) + " " + str(time_frame) + " " + str(min_tolerance[msg.arbitration_id]/2))
-                        elif time_frame < min_tolerance[msg.arbitration_id]:
+                if msg.dlc != 0 and (msg.arbitration_id not in ignore_next_msg):
+                    if msg.arbitration_id in last_timestamp:
+                        time_frame = msg.timestamp - last_timestamp[msg.arbitration_id]
+                        if msg.arbitration_id not in min_tolerance:
                             min_tolerance[msg.arbitration_id] = time_frame
+                        else:
+                            if time_frame < (min_tolerance[msg.arbitration_id]/2):
+                                logging.error("ATTACK detected: i=" + str(i) + " " + str(msg) + " " + str(time_frame) + " " + str(min_tolerance[msg.arbitration_id]/2))
+                                min_tolerance[msg.arbitration_id] = time_frame
+                            elif time_frame < min_tolerance[msg.arbitration_id]:
+                                min_tolerance[msg.arbitration_id] = time_frame
 
-                last_timestamp[msg.arbitration_id] = msg.timestamp
+                    last_timestamp[msg.arbitration_id] = msg.timestamp
+
+                elif msg.dlc != 0 and (msg.arbitration_id in ignore_next_msg):
+                    del ignore_next_msg[msg.arbitration_id]
+                else:
+                    ignore_next_msg[msg.arbitration_id] = True
+
                 # logging.info(msg.arbitration_id)
             i+=1
-        print(min_tolerance)
-        # start IDS 
-        while True:
-            msg = self.bus.recv(60)
-            if msg is None:
-                logging.info('No message has been received')
-                return
-            else:
-                # logging.info(str(msg)+ ' ' + str(i))
-
-                
-                if msg.arbitration_id in last_timestamp and msg.arbitration_id in min_tolerance:
-                    time_frame = msg.timestamp - last_timestamp[msg.arbitration_id]
-                    if time_frame < min_tolerance[msg.arbitration_id]:
-                        logging.error("ATTACK detected: i=" + str(i))
-                    # else:
-                    #     logging.info("OK " + str(i))
-                    last_timestamp[msg.arbitration_id] = time_frame
-            i+=1
-        return
 
 
 if __name__ == '__main__':
@@ -173,4 +158,4 @@ if __name__ == '__main__':
 
     # start threads
     IDS.start()
-    CANbus.start()
+    # CANbus.start()
